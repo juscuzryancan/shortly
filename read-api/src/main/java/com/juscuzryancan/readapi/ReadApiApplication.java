055518d21e666aef8ce591eafa6dfc9606597da0
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-09-23 11:50:09 -0700

    bootstraps read api

---
+++ read-api/src/main/java/com/juscuzryancan/readapi/ReadApiApplication.java
@@ -0,0 +1,13 @@
+package com.juscuzryancan.readapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadApiApplication.class, args);
+	}
+
+}
---
+++ read-api/src/test/java/com/juscuzryancan/readapi/ReadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juscuzryancan.readapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
