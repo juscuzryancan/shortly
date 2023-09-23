f65a2fb65a73ca62c497b166f90d28456fe8db3f
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-09-23 12:36:26 -0700

    adds simple getmapping into readcontroller

---
+++ read-api/src/main/java/com/juscuzryancan/controllers/ReadController.java
@@ -0,0 +1,24 @@
+package com.juscuzryancan.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ReadController {
+
+  @GetMapping("/")
+  public ResponseEntity<String> example() {
+    return new ResponseEntity("hello", HttpStatus.OK);
+  }
+
+}
