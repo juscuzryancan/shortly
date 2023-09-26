456b0f109ea04cf32432d381293a777d92518019
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-09-25 20:23:37 -0700

    implements shorturlnotfoundexception

---
+++ redirect-service/src/main/java/com/juscuzryancan/redirectservice/ShortURLNotFoundException.java
@@ -0,0 +1,7 @@
+package com.juscuzryancan.redirectservice;
+
+public class ShortURLNotFoundException extends Exception{
+    public ShortURLNotFoundException(String message) {
+        super(message);
+    }
+}
