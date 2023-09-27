fd314c454a542b68d98ff3b569e64df47ba15136
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-09-26 20:22:11 -0700

    adds shortener-service

---
+++ shortener-service/src/test/java/com/example/juscuzyrancan/shortenerservice/ShortenerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juscuzyrancan.shortenerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
