2514fa72306a142022a7f731d385e3c7bcf75747
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-09-27 20:19:32 -0700

    creates new urlservice

---
+++ url-service/src/test/java/com/juscuzryancan/urlservice/UrlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juscuzryancan.urlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
