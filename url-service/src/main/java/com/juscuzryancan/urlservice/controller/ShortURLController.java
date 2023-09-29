f409c354c23d0d0d37d2f096642ba5bdbee52c33
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-09-28 17:03:17 -0700

    simplifies endpoints

--- url-service/src/main/java/com/juscuzryancan/urlservice/controller/ShortURLController.java
+++ url-service/src/main/java/com/juscuzryancan/urlservice/controller/ShortURLController.java
@@ -2,12 +2,16 @@ package com.juscuzryancan.urlservice.controller;
 
 import com.juscuzryancan.urlservice.dto.ShortURLRequest;
 import com.juscuzryancan.urlservice.service.URLShortenerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.security.NoSuchAlgorithmException;
+
 @RestController
+@RequestMapping("/api/v1")
+@EnableCaching
 public class ShortURLController {
 
     private URLShortenerService shortenerService;
@@ -16,19 +20,15 @@ public class ShortURLController {
         this.shortenerService = shortenerService;
     }
 
-    @GetMapping("/{shortURL}")
+    @GetMapping("/shortURL/{shortURL}")
     public RedirectView redirect(@PathVariable("shortURL") String shortURL) {
-        String URL = shortenerService.getOriginalLink();
+        String URL = shortenerService.getOriginalLink(shortURL);
         return new RedirectView(URL);
     }
 
-    @PostMapping("/api/shorturl")
-    public ResponseEntity<String> createShortURL(@RequestBody ShortURLRequest request) {
-        try {
-            String shortURL = shortenerService.createShortURL(request.getURL());
-            return ResponseEntity.ok(shortURL);
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
+    @PostMapping("/data/shorten")
+    public ResponseEntity<String> createShortURL(@RequestBody ShortURLRequest request) throws NoSuchAlgorithmException {
+        String shortURL = shortenerService.createShortURL(request.getURL());
+        return ResponseEntity.ok(shortURL);
     }
 }
