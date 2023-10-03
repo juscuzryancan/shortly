4c6697ce7d6bca0990bdfbe2188db6c1f73b6b0f
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-10-02 19:58:40 -0700

    implements basic securityconfig to secure all endpoints

---
+++ api-gateway/src/main/java/com/juscuzryancan/apigateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.juscuzryancan.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated()
+                )
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return http.build();
+    }
+}
